b280f710ac332949f69838f3cd2a0f33fb86ae1b
daorejuela1 <daorejuela1@outlook.com>
2021-05-02 04:34:38 +0000

    Fixed NavBar style

--- app/helpers/shared_helper.rb
+++ app/helpers/shared_helper.rb
@@ -1,9 +1,8 @@
 module SharedHelper
   def display_tenants_helper(current_user)
     form_with url: edit_account_path,  method: :post do |form|
-      concat form.label :account
       concat form.select :account, current_user.tenant_list
-      concat form.submit("Change Team", class: "btn btn-primary")
+      concat form.submit("Change Team", class: "form-inline btn btn-primary ml-2")
     end
   end
 
